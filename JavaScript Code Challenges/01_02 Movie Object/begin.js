592e4594f49e6da5740a5207297d22e03413ccc7
Paul Rosero <paul.rosero23@gmail.com>
2021-04-08 18:04:40 -0400

    add class Movie and code to complete the README

--- JavaScript Code Challenges/01_02 Movie Object/begin.js
+++ JavaScript Code Challenges/01_02 Movie Object/begin.js
@@ -1,3 +1,16 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+  }
+}
