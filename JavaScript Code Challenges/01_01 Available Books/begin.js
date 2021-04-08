ffd121d2feea43af9bfee2228a0834e3ccaaa39e
Paul Rosero <paul.rosero23@gmail.com>
2021-04-08 17:04:45 -0400

    add code without looking at the answer

--- JavaScript Code Challenges/01_01 Available Books/begin.js
+++ JavaScript Code Challenges/01_01 Available Books/begin.js
@@ -1,3 +1,33 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  } 
+  get availability() {
+    return this.getAvailability 
+  }
+
+  getAvailability() {
+    if (this.numCopies == 0) {
+      return "Out of stock";
+    } else if ( this.numCopies > 0 && this.numCopies < 10) {
+      return "Low stock";
+    } else {
+      return "In stock";
+    }
+  }
+
+  sell(numSold = 1) {
+    this.numCopies -= numSold
+  }
+
+  restock(restockCopies = 5) {
+    this.numCopies += restockCopies
+  }
+
+}
