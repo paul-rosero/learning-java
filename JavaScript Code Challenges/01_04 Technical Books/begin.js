051972802979b068df1903335f3882791633627a
Paul Rosero <paul.rosero23@gmail.com>
2021-04-09 12:40:23 -0400

    add code to pass the challenge

--- JavaScript Code Challenges/01_04 Technical Books/begin.js
+++ JavaScript Code Challenges/01_04 Technical Books/begin.js
@@ -4,32 +4,57 @@ class Book {
     this.author = author;
     this.ISBN = ISBN;
     this.numCopies = numCopies;
-  }
-
-  // Getter
+  } 
   get availability() {
-    return this.getAvailability();
+    return this.getAvailability()
   }
 
-  // Method
   getAvailability() {
-    if (this.numCopies === 0) {
+    if (this.numCopies == 0) {
       return "Out of stock";
-    } else if (this.numCopies < 10) {
+    } else if ( this.numCopies > 0 && this.numCopies < 10) {
       return "Low stock";
+    } else {
+      return "In stock";
     }
-    return "In stock";
   }
 
-  sell(numCopiesSold = 1) {
-    this.numCopies -= numCopiesSold;
+  sell(numSold = 1) {
+    this.numCopies -= numSold
   }
 
-  restock(numCopiesStocked = 5) {
-    this.numCopies += numCopiesStocked;
+  restock(restockCopies = 5) {
+    this.numCopies += restockCopies
+  }
+
+}
+
+class TechnicalBook extends Book {
+  constructor(title, author, ISBN, numCopies, version) {
+    super(title, author, ISBN, numCopies);
+    this.version = version
+  }
+  getEdition() {
+    return `The current version of this book is ${this.version}`
   }
 }
 
 /*
   WRITE YOUR SOLUTION HERE
 */
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.availability);
+HungerGames.restock(12);
+console.log(HungerGames.availability);
+HungerGames.sell(17);
+console.log(HungerGames.availability);
+
+const CrackingTheCodingInterview = new TechnicalBook(
+  "Cracking The Coding Interview",
+  "Gayle Laackmann McDowell",
+  1209123,
+  7,
+  "2.3"
+);
+console.log(CrackingTheCodingInterview.availability);
+console.log(CrackingTheCodingInterview.getEdition());
