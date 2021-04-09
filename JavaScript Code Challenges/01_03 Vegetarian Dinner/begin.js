55c9e9a6515793b6f79c6f4c972864bda361e5b1
Paul Rosero <paul.rosero23@gmail.com>
2021-04-09 12:22:38 -0400

    write code to do what the challenge says

--- JavaScript Code Challenges/01_03 Vegetarian Dinner/begin.js
+++ JavaScript Code Challenges/01_03 Vegetarian Dinner/begin.js
@@ -1,3 +1,37 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+veganMenu([
+  {
+    name: "Chicken parmesan",
+    isVegetarian: false,
+  },
+  {
+    name: "Penne a la vodka",
+    isVegetarian: true,
+  },
+  {
+    name: "Mushroom risotto",
+    isVegetarian: true,
+  },
+  {
+    name: "Veal saltambuca",
+    isVegetarian: false,
+  },
+  {
+    name: "Filet mignon",
+    isVegetarian: false,
+  },
+]);
+
+function veganMenu(menu) {
+  const nodeMenu = document.getElementById("menu");
+  console.log(nodeMenu)
+  const veggieMenu = menu.filter((plate) => plate.isVegetarian === true);
+  
+  veggieMenu.forEach((dish) => {
+    let menuLi = document.createElement("li");
+    menuLi.textContent = dish.name;
+    nodeMenu.appendChild(menuLi);
+  });
+}
