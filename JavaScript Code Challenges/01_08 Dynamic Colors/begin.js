16f9c655bbcc64a8a926253a31c959ba6b3de755
Paul Rosero <paul.rosero23@gmail.com>
2021-04-10 18:08:27 -0400

    write code that solves challenge

--- JavaScript Code Challenges/01_08 Dynamic Colors/begin.js
+++ JavaScript Code Challenges/01_08 Dynamic Colors/begin.js
@@ -1,3 +1,14 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+function changeBackgroundColor(color) {
+  return function() { 
+    document.body.style.backgroundColor = color 
+  };
+};
+
+document.getElementById("blue").addEventListener("click", changeBackgroundColor("blue"));
+document.getElementById("green").addEventListener("click", changeBackgroundColor("green"));
+document.getElementById("pink").addEventListener("click", changeBackgroundColor("pink"));
+
+
