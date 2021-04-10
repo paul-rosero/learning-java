0b3758de568fb0293a9cb47205cd331184c71096
Paul Rosero <paul.rosero23@gmail.com>
2021-04-10 14:17:19 -0400

    add 2 diff codes that solve the challenge

--- JavaScript Code Challenges/01_07 Classroom Attendance/begin.js
+++ JavaScript Code Challenges/01_07 Classroom Attendance/begin.js
@@ -1,3 +1,36 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+  
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+// function getStudents(classroom) {
+//   if (classroom.hasTeachingAssistant === true) {
+//     return classroom.classList.slice(2);
+//   } else {
+//     return classroom.classList.slice(1);
+//   };
+// }
+
+const classroom1 = {
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+};
+
+const classroom2 = {
+  hasTeachingAssistant: true,
+  classList: ["James", "Mike", "Roman", "Lisa", "Omair", "Lukas"]
+};
+
+console.log("classroom1", getStudents(classroom1));
+console.log("classroom2",getStudents(classroom2));
