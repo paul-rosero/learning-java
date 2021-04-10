21e34c20c98b4779d25023e2f1df3cb6e99234cf
Paul Rosero <paul.rosero23@gmail.com>
2021-04-10 11:11:12 -0400

    add code to do what challenge requires

--- JavaScript Code Challenges/01_05 Private Object/begin.js
+++ JavaScript Code Challenges/01_05 Private Object/begin.js
@@ -1,3 +1,15 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+const admin = Symbol("adminUser")
+
+const user = {
+  admin: true,
+  [Symbol("username")]: "myusername",
+  [Symbol("password")]: "mypassword",
+  age: 21
+}
+console.log(user.adminUser)
+console.log(user.username);
+console.log(user.password);
+console.log(user.age)
