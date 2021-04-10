fc5de0aba35159cc3ab75a9a37143ee3fca0f77d
Paul Rosero <paul.rosero23@gmail.com>
2021-04-10 11:55:52 -0400

    code to solve challenge

--- JavaScript Code Challenges/01_06 Ticking Time/begin.js
+++ JavaScript Code Challenges/01_06 Ticking Time/begin.js
@@ -1,3 +1,9 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+const clock = document.getElementById("clock");
+setInterval(() => {
+  clock.innerText = new Date().toLocaleTimeString();
+}, 1000);
+
+console.log(clock)
