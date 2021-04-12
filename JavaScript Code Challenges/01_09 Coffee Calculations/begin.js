bc3be308f03c70eb1228fb6b0bc0e9db8c8ca911
Paul Rosero <paul.rosero23@gmail.com>
2021-04-12 19:02:10 -0400

    wrote code to solve the challenge

--- JavaScript Code Challenges/01_09 Coffee Calculations/begin.js
+++ JavaScript Code Challenges/01_09 Coffee Calculations/begin.js
@@ -1,3 +1,10 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function totalPrice(arr) {
+  const total = arr.reduce((total, currentValue) => (total += currentValue));
+  return `The total bill is $${total * 1.25}`;
+};
+
+console.log(totalPrice([2,4,5]));
