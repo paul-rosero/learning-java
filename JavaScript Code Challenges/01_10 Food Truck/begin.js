0a5d9dab2cf10219f71adc193fb72b4316e9be06
Paul Rosero <paul.rosero23@gmail.com>
2021-04-13 11:23:20 -0400

    add code that solves the challenge

--- JavaScript Code Challenges/01_10 Food Truck/begin.js
+++ JavaScript Code Challenges/01_10 Food Truck/begin.js
@@ -1,3 +1,22 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+const menus = [['Tacos', 'Burgers'], ['Pizza'], ['Burgers', 'Fries']];
+
+function masterMenu (menus) {
+  menus = menus.flat();
+  const masterMenu = new Set();
+  menus.forEach(plate => {
+    masterMenu.add(plate);
+  });
+
+  const ul = document.getElementById("combined-menu");
+  for (const dish of masterMenu) {
+    const li = document.createElement("li");
+    li.innerText = dish;
+    ul.appendChild(li);
+  }
+};
+
+masterMenu(menus);
