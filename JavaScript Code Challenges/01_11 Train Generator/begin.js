320ee742f475f0b40abf6d586ffa414803d30419
Paul Rosero <paul.rosero23@gmail.com>
2021-04-14 13:24:52 -0400

    add code that solves the challenge

--- JavaScript Code Challenges/01_11 Train Generator/begin.js
+++ JavaScript Code Challenges/01_11 Train Generator/begin.js
@@ -1,3 +1,24 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function * getStop() {
+  yield "Poughkeepsie";
+  yield "Newburgh";
+  yield "Peekskill";
+  yield "Yonkers";
+  yield "Bronx";
+  yield "Grand Central";
+}
+
+const nycTrainLine = getStop();
+const nextButton = document.getElementById("next-stop");
+nextButton.addEventListener("click", () => {
+  let currStop = nycTrainLine.next();
+  if (currStop.done) {
+    console.log("We made it!");
+    nextButton.setAttribute("disabled", true);
+  } else {
+    console.log(currStop.value);
+  }
+});
